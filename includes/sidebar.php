cb697a168632986602de44a6995bafd852e66566
diyavora2208 <diyavora2208@gmail.com>
2025-07-15 14:21:13 +0530

    Add files via upload

---
+++ includes/sidebar.php
@@ -0,0 +1,37 @@
+<aside class="menu-sidebar d-none d-lg-block">
+            <div class="logo">
+        
+                   <h3>Notice Board</h3>
+            </div>
+            <div class="menu-sidebar__content js-scrollbar1">
+                <nav class="navbar-sidebar">
+                    <ul class="list-unstyled navbar__list">
+                        <li>
+                            <a href="dashboard.php"  style="color: blue">
+                                <i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                        </li>
+       <li>
+                            <a href="category.php"  style="color: blue">
+                                <i class="fas fa-file-alt"></i>Category</a>
+                        </li>
+
+
+     <li>
+                            <a href="add-notices.php"  style="color: blue">
+                                <i class="fa fa-file"></i>Add Notice</a>
+                        </li>
+   <li>
+                            <a href="manage-notices.php"  style="color: blue">
+                                <i class="fa fa-files-o"></i>Manage Notices</a>
+                        </li>
+
+                        
+                      <li>
+                            <a href="bwdates-reports.php"  style="color: blue">
+                                <i class="fas fa-copy"></i>Notice B/w Dates</a>
+                        </li>  
+                       
+                    </ul>
+                </nav>
+            </div>
+        </aside>
